805dc6aa03ba9d273f920eb230bb5ca1c5afb27b
Vahid Lari <vahid.lari@gmail.com>
2025-08-17 01:28:23 +0200

    Minor updates in the way to handle the Pipfile

--- tools/docker/generate-lock.sh
+++ tools/docker/generate-lock.sh
@@ -5,20 +5,10 @@ set -e
 
 echo "Generating pip.lock file..."
 
-# Create a temporary virtual environment
-python3.11 -m venv temp_venv
-source temp_venv/bin/activate
-
-# Upgrade pip
-pip install --upgrade pip
-
-# Install packages and generate lock file
-pip install -r requirements.txt
-pip freeze > pip.lock
-
-# Clean up
-deactivate
-rm -rf temp_venv
+# Convert requirements.txt to Pipfile
+pipenv install -r requirements.txt
+# Then generate lock file
+pipenv lock
 
 echo "pip.lock file generated successfully!"
 echo "You can now build the Docker image with reproducible dependencies."
